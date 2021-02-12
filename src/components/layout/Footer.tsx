bda74b64420a9a5ee431d2c32f734e1729e0da90
Amanda Kendal-Brown <ajkendal@neiu.edu>
2021-02-12 17:42:00 -0600

    DEV-155 WIP
    updates to Footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -8,66 +8,56 @@ import Facebook from '../../images/icons-footer/facebook-f-brands.svg';
 import Linkedin from '../../images/icons-footer/linkedin-in-brands.svg';
 import Envelope from '../../images/icons-footer/envelope-regular.svg';
 import { ReactComponent as Logo } from '../../images/logoPlain.svg';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const Footer = () => (
   <Navbar className="nav justify-content-md-center" sticky="bottom">
-    <div className="row justify-content-center">
-      <div className="col">
+    <Row>
+      <Col md={3}>
         <Nav.Link as={Link} to="/">
           HOME
         </Nav.Link>
         <Nav.Link as={Link} to="/who-we-are">
           WHO WE ARE
         </Nav.Link>
-      </div>
-      <div className="col">
-        <div className="row justify-content-center">
+      </Col>
+      <Col md={6}>
+        <Row className="justify-content-md-center">
           <Navbar.Brand href="/">
             <Logo height="60px" width="70px" />
           </Navbar.Brand>
-        </div>
-        <div className="row">
-          <div className="col-2">
-            <Nav.Link href="http://www.facebook.com/chiartistguide">
-              <FooterLogo src={Facebook} />
-            </Nav.Link>
-          </div>
-          <div className="col-2">
-            <Nav.Link href="http://www.facebook.com/chiartistguide">
-              <FooterLogo src={Facebook} />
-            </Nav.Link>
-          </div>
-          <div className="col-2">
-            <Nav.Link href="https://www.linkedin.com/company/chicago-artist-guide">
-              <FooterLogo src={Linkedin} />
-            </Nav.Link>
-          </div>
-          <div className="col-2">
-            <Nav.Link href="https://www.linkedin.com/company/chicago-artist-guide">
-              <FooterLogo src={Linkedin} />
-            </Nav.Link>
-          </div>
-          <div className="col-2">
-            <Nav.Link href="mailto:contact@chicagoartistguide.org">
-              <FooterLogo src={Envelope} />
-            </Nav.Link>
-          </div>
-          <div className="col-2">
-            <Nav.Link href="mailto:contact@chicagoartistguide.org">
-              <FooterLogo src={Envelope} />
-            </Nav.Link>
-          </div>
-        </div>
-      </div>
-      <div className="col">
+        </Row>
+        <Row className="justify-content-center">
+          <Nav.Link href="http://www.facebook.com/chiartistguide">
+            <FooterLogo src={Facebook} />
+          </Nav.Link>
+          <Nav.Link href="http://www.facebook.com/chiartistguide">
+            <FooterLogo src={Facebook} />
+          </Nav.Link>
+          <Nav.Link href="https://www.linkedin.com/company/chicago-artist-guide">
+            <FooterLogo src={Linkedin} />
+          </Nav.Link>
+          <Nav.Link href="https://www.linkedin.com/company/chicago-artist-guide">
+            <FooterLogo src={Linkedin} />
+          </Nav.Link>
+          <Nav.Link href="mailto:contact@chicagoartistguide.org">
+            <FooterLogo src={Envelope} />
+          </Nav.Link>
+          <Nav.Link href="mailto:contact@chicagoartistguide.org">
+            <FooterLogo src={Envelope} />
+          </Nav.Link>
+        </Row>
+      </Col>
+      <Col md={3}>
         <Nav.Link as={Link} to="/faq">
           FAQ
         </Nav.Link>
         <Nav.Link as={Link} to="/terms-of-service">
           TERMS OF SERVICE
         </Nav.Link>
-      </div>
-    </div>
+      </Col>
+    </Row>
   </Navbar>
 );
 
