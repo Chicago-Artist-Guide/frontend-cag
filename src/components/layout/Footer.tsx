da384817d18b53194771965d37fc8cef5289e54b
Amanda Kendal-Brown <ajkendal@neiu.edu>
2021-02-12 17:26:41 -0600

    DEV-155, WIP
    Added baseline for new footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -7,32 +7,67 @@ import Image from 'react-bootstrap/Image';
 import Facebook from '../../images/icons-footer/facebook-f-brands.svg';
 import Linkedin from '../../images/icons-footer/linkedin-in-brands.svg';
 import Envelope from '../../images/icons-footer/envelope-regular.svg';
+import { ReactComponent as Logo } from '../../images/logoPlain.svg';
 
 const Footer = () => (
-  <Navbar className="nav" expand="sm" sticky="bottom">
-    <Nav className="m-auto">
-      <Nav.Link as={Link} to="/">
-        HOME
-      </Nav.Link>
-      <Nav.Link as={Link} to="/who-we-are">
-        WHO WE ARE
-      </Nav.Link>
-      <Nav.Link as={Link} to="/terms-of-service">
-        TERMS OF SERVICE
-      </Nav.Link>
-      <Nav.Link as={Link} to="/faq">
-        FAQ
-      </Nav.Link>
-      <Nav.Link href="http://www.facebook.com/chiartistguide">
-        <FooterLogo src={Facebook} />
-      </Nav.Link>
-      <Nav.Link href="https://www.linkedin.com/company/chicago-artist-guide">
-        <FooterLogo src={Linkedin} />
-      </Nav.Link>
-      <Nav.Link href="mailto:contact@chicagoartistguide.org">
-        <FooterLogo src={Envelope} />
-      </Nav.Link>
-    </Nav>
+  <Navbar className="nav justify-content-md-center" sticky="bottom">
+    <div className="row justify-content-center">
+      <div className="col">
+        <Nav.Link as={Link} to="/">
+          HOME
+        </Nav.Link>
+        <Nav.Link as={Link} to="/who-we-are">
+          WHO WE ARE
+        </Nav.Link>
+      </div>
+      <div className="col">
+        <div className="row justify-content-center">
+          <Navbar.Brand href="/">
+            <Logo height="60px" width="70px" />
+          </Navbar.Brand>
+        </div>
+        <div className="row">
+          <div className="col-2">
+            <Nav.Link href="http://www.facebook.com/chiartistguide">
+              <FooterLogo src={Facebook} />
+            </Nav.Link>
+          </div>
+          <div className="col-2">
+            <Nav.Link href="http://www.facebook.com/chiartistguide">
+              <FooterLogo src={Facebook} />
+            </Nav.Link>
+          </div>
+          <div className="col-2">
+            <Nav.Link href="https://www.linkedin.com/company/chicago-artist-guide">
+              <FooterLogo src={Linkedin} />
+            </Nav.Link>
+          </div>
+          <div className="col-2">
+            <Nav.Link href="https://www.linkedin.com/company/chicago-artist-guide">
+              <FooterLogo src={Linkedin} />
+            </Nav.Link>
+          </div>
+          <div className="col-2">
+            <Nav.Link href="mailto:contact@chicagoartistguide.org">
+              <FooterLogo src={Envelope} />
+            </Nav.Link>
+          </div>
+          <div className="col-2">
+            <Nav.Link href="mailto:contact@chicagoartistguide.org">
+              <FooterLogo src={Envelope} />
+            </Nav.Link>
+          </div>
+        </div>
+      </div>
+      <div className="col">
+        <Nav.Link as={Link} to="/faq">
+          FAQ
+        </Nav.Link>
+        <Nav.Link as={Link} to="/terms-of-service">
+          TERMS OF SERVICE
+        </Nav.Link>
+      </div>
+    </div>
   </Navbar>
 );
 
