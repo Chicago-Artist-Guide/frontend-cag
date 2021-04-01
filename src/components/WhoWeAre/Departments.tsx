8362835fb64e03250d27be900ba7cbbb8aef17d3
Amanda Kendal-Brown <ajkendal@neiu.edu>
2021-04-01 11:17:50 -0500

    DES-6
    UPDATE to Sections, next step is to fix and alter the Team.tsx file

--- src/components/WhoWeAre/Departments.tsx
+++ src/components/WhoWeAre/Departments.tsx
@@ -1,30 +1,69 @@
 import React from 'react';
 import styled from 'styled-components';
-//import Row from 'react-bootstrap/Row';
-//import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Accordion from 'react-bootstrap/Accordion';
-
-import Bios from './Bios';
+import bios from './bios';
+import Team from './Team';
 
 const Department = () => {
   return (
     <AccordionSection className="container">
       <Accordion defaultActiveKey="0">
         <Accordion.Toggle className="accordionHeader row" eventKey="0">
-          <h2>test</h2>
+          <h2>LEADERSHIP</h2>
         </Accordion.Toggle>
         <Accordion.Collapse eventKey="0">
-          <p>SOME Text</p>
+          <Row>
+            {bios['board'].map(who => (
+              <Col md={3}>
+                <Team
+                  image={who.image}
+                  name={who.name}
+                  pronoun={who.pronouns}
+                />
+              </Col>
+            ))}
+          </Row>
         </Accordion.Collapse>
 
         <Accordion.Toggle className="accordionHeader row" eventKey="1">
-          <h2>ADVISORY BOARD</h2>
+          <h2>BUSINESS OPERATIONS</h2>
         </Accordion.Toggle>
         <Accordion.Collapse eventKey="1">
-          <p>SOME Text</p>
+          <Row>
+            {bios['operations'].map(who => (
+              <Col md={3}>
+                <Team
+                  image={who.image}
+                  name={who.name}
+                  pronoun={who.pronouns}
+                />
+              </Col>
+            ))}
+          </Row>
         </Accordion.Collapse>
 
-        {console.log(Bios)}
+        <Accordion.Toggle className="accordionHeader row" eventKey="2">
+          <h2>DEVELOPMENT</h2>
+        </Accordion.Toggle>
+        <Accordion.Collapse eventKey="2">
+          <Row>
+            {bios['technical'].map(who => (
+              <Col md={3}>
+                <Team
+                  image={who.image}
+                  name={who.name}
+                  pronoun={who.pronouns}
+                />
+              </Col>
+            ))}
+          </Row>
+        </Accordion.Collapse>
+
+        {console.log(bios['board'])}
+        {console.log(bios['operations'])}
+        {console.log(bios['technical'])}
       </Accordion>
     </AccordionSection>
   );
