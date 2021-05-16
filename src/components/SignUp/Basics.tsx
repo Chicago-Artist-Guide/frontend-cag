51213d466f0d353337b00284229d133704c28c3e
Alex Jewell <al3xj3w3ll@gmail.com>
2021-05-16 17:12:59 -0500

    Fix

--- src/components/SignUp/Basics.tsx
+++ src/components/SignUp/Basics.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
