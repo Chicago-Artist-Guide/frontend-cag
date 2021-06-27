50ad920d6ddc4f8b799ae893290e62a9d4ccbd86
Alex Jewell <al3xj3w3ll@gmail.com>
2021-06-26 21:16:14 -0500

    Actor info 2 page initial components

--- src/components/SignUp/ActorInfo2.tsx
+++ src/components/SignUp/ActorInfo2.tsx
@@ -1,21 +1,179 @@
 import React from 'react';
+import styled from 'styled-components';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import Form from 'react-bootstrap/Form';
+import Checkbox from '../../genericComponents/Checkbox';
+import { colors, fonts } from '../../theme/styleVars';
+import yellow_blob from '../../images/yellow_blob_2.svg';
+
+const ageRanges = [
+  '19 and younger',
+  '20-24',
+  '25-29',
+  '30-34',
+  '34-39',
+  '40-45',
+  '46-55',
+  '56+'
+];
+
+const genders = [
+  'Cis Female',
+  'Cis Male',
+  'Trans Female',
+  'Trans Male',
+  'Non Binary/Agender',
+  'I choose not to respond'
+];
+
+const genderRoles = [
+  'Women',
+  'Men',
+  'Neither'
+];
 
 const ActorInfo2: React.FC<{
   setForm: any;
   formData: any;
 }> = props => {
+  const { setForm } = props;
+
   return (
     <Container>
       <Row>
-        <Col lg="12">
+        <Col lg="8">
           <h1>Let's Get Some Details</h1>
+          <Row>
+            <Col lg="12">
+              <Form.Group>
+                <CAGLabel>Height</CAGLabel>
+                <Container>
+                  <Row>
+                    <Col lg="3">
+                      <Form.Control
+                        aria-label="height feet"
+                        as="select"
+                        name="actorInfo2HeightFt"
+                        onChange={setForm}
+                      >
+                        {[0, 1, 2, 3, 4, 5, 6, 7].map(ft => (
+                          <option value={ft}>{ft} ft</option>
+                        ))}
+                      </Form.Control>
+                    </Col>
+                    <Col lg="3">
+                      <Form.Control
+                        aria-label="height inches"
+                        as="select"
+                        name="actorInfo2HeightIn"
+                        onChange={setForm}
+                      >
+                        {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map(inches => (
+                          <option value={inches}>{inches} in</option>
+                        ))}
+                      </Form.Control>
+                    </Col>
+                    <Col lg="6">
+                      <Checkbox
+                        checked={false}
+                        fieldType="checkbox"
+                        label="I do not wish to answer"
+                        name="actorInfo2HeightNoAnswer"
+                        onChange={setForm}
+                      />
+                    </Col>
+                  </Row>
+                </Container>
+              </Form.Group>
+              <Form.Group>
+                <CAGLabel>Age Range - Private</CAGLabel>
+                <p>Select up to 3 ranges</p>
+                {ageRanges.map(ageRange => (
+                  <Checkbox
+                    checked={false}
+                    fieldType="checkbox"
+                    label={ageRange}
+                    name="actorInfo2AgeRanges"
+                    onChange={setForm}
+                  />
+                ))}
+              </Form.Group>
+              <Form.Group>
+                <CAGLabel>Gender Identity - Private</CAGLabel>
+                <p>
+                  First, choose your gender identity - additional options may be presented for casting purposes. 
+                  If other, please select the option with which you most closely identify for casting purposes.
+                </p>
+                <Form.Control
+                  as="select"
+                  defaultValue=""
+                  name="actorInfo2Gender"
+                  onChange={setForm}
+                >
+                  {genders.map(g => (
+                    <option value={g}>{g}</option>
+                  ))}
+                </Form.Control>
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col lg="6">
+              <Form.Group>
+                <CAGLabel>I would also be comfortable playing roles usually played by:</CAGLabel>
+                {genderRoles.map(g => (
+                  <Checkbox
+                    checked={false}
+                    fieldType="checkbox"
+                    label={g}
+                    name="actorInfo2GenderRoles"
+                    onChange={setForm}
+                  />
+                ))}
+              </Form.Group>
+            </Col>
+            <Col lg="6">
+              <Form.Group>
+                <CAGLabel>I would be comfortable playing a character through all phases of their transition:</CAGLabel>
+                <Checkbox
+                  checked={false}
+                  fieldType="checkbox"
+                  label="Yes"
+                  name="actorInfo2GenderTransition"
+                  onChange={setForm}
+                />
+                <Checkbox
+                  checked={false}
+                  fieldType="checkbox"
+                  label="No"
+                  name="actorInfo2GenderTransition"
+                  onChange={setForm}
+                />
+              </Form.Group>
+            </Col>
+          </Row>
         </Col>
+        <ImageCol lg="4">
+          <Image alt="" src={yellow_blob} />
+        </ImageCol>
       </Row>
     </Container>
   );
 };
+
+const ImageCol = styled(Col)`
+  display: flex;
+  max-height: 100%;
+  max-width: 100%;
+`;
+
+const CAGLabel = styled(Form.Label)`
+  color: ${colors.mainFont};
+  font-family: ${fonts.mainFont};
+  font-size: 20px;
+`;
 
 export default ActorInfo2;
