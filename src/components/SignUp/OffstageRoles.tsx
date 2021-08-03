3425d22dc83e9694bfcbdd4d97825b1cfa24b2b4
Alex Jewell <alex.jewell@polysign.io>
2021-08-03 12:12:52 -0500

    [DEV-143] Sign-Up Flow: Offstage Roles (#31)

--- src/components/SignUp/OffstageRoles.tsx
+++ src/components/SignUp/OffstageRoles.tsx
@@ -1,22 +1,298 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
+import styled from 'styled-components';
+import { fonts } from '../../theme/styleVars';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import Container from 'react-bootstrap/Container';
+import GenericAccordion from '../../genericComponents/Accordion';
+import Checkbox from '../../genericComponents/Checkbox';
+import yellow_blob_1 from '../../images/yellow_blob_1.svg';
 
 const OffstageRoles: React.FC<{
   setForm: any;
   formData: any;
 }> = props => {
+  const { formData, setForm } = props;
+
+  const {
+    offstageRolesGeneral,
+    offstageRolesProduction,
+    offstageRolesScenicAndProperties,
+    offstageRolesLighting,
+    offstageRolesSound,
+    offstageRolesHairMakeupCostumes
+  } = formData;
+
+  const offstageRolesObj = {
+    general: {
+      sectionStateValue: offstageRolesGeneral,
+      sectionStateName: 'offstageRolesGeneral',
+      textHeader: 'General',
+      checkboxes: [
+        {
+          label: 'Directing',
+          value: 'Directing'
+        },
+        {
+          label: 'Violence / Fight Design',
+          value: 'Violence / Fight Design'
+        },
+        {
+          label: 'Intimacy Design',
+          value: 'Intimacy Design'
+        },
+        {
+          label: 'Choreography',
+          value: 'Choreography'
+        },
+        {
+          label: 'Casting',
+          value: 'Casting'
+        },
+        {
+          label: 'Dramaturgy',
+          value: 'Dramaturgy'
+        },
+        {
+          label: 'Dialect Coaching',
+          value: 'Dialect Coaching'
+        },
+        {
+          label: 'Musical Directing',
+          value: 'Musical Directing'
+        }
+      ]
+    },
+    production: {
+      sectionStateValue: offstageRolesProduction,
+      sectionStateName: 'offstageRolesProduction',
+      textHeader: 'Production',
+      checkboxes: [
+        {
+          label: 'Stage Management',
+          value: 'Stage Management'
+        },
+        {
+          label: 'Production Management',
+          value: 'Production Management'
+        },
+        {
+          label: 'Board Op',
+          value: 'Board Op'
+        },
+        {
+          label: 'Run Crew',
+          value: 'Run Crew'
+        }
+      ]
+    },
+    scenicAndProperties: {
+      sectionStateValue: offstageRolesScenicAndProperties,
+      sectionStateName: 'offstageRolesScenicAndProperties',
+      textHeader: 'Scenic & Properties',
+      checkboxes: [
+        {
+          label: 'Set Design',
+          value: 'Set Design'
+        },
+        {
+          label: 'Technical Direction',
+          value: 'Technical Direction'
+        },
+        {
+          label: 'Properties Designer',
+          value: 'Properties Designer'
+        },
+        {
+          label: 'Scenic Carpentry',
+          value: 'Scenic Carpentry'
+        },
+        {
+          label: 'Charge Artist',
+          value: 'Charge Artist'
+        }
+      ]
+    },
+    lighting: {
+      sectionStateValue: offstageRolesLighting,
+      sectionStateName: 'offstageRolesLighting',
+      textHeader: 'Lighting',
+      checkboxes: [
+        {
+          label: 'Lighting Design',
+          value: 'Lighting Design'
+        },
+        {
+          label: 'Projection Design',
+          value: 'Projection Design'
+        },
+        {
+          label: 'Special Effect Design',
+          value: 'Special Effect Design'
+        },
+        {
+          label: 'Master Electrician',
+          value: 'Master Electrician'
+        }
+      ]
+    },
+    sound: {
+      sectionStateValue: offstageRolesSound,
+      sectionStateName: 'offstageRolesSound',
+      textHeader: 'Sound',
+      checkboxes: [
+        {
+          label: 'Sound Design',
+          value: 'Sound Design'
+        },
+        {
+          label: 'Sound Mixer / Engineer',
+          value: 'Sound Mixer / Engineer'
+        }
+      ]
+    },
+    hairMakeupCostumes: {
+      sectionStateValue: offstageRolesHairMakeupCostumes,
+      sectionStateName: 'offstageRolesHairMakeupCostumes',
+      textHeader: 'Hair, Makeup, Costumes',
+      checkboxes: [
+        {
+          label: 'Costume Design',
+          value: 'Costume Design'
+        },
+        {
+          label: 'Hair & Wig Design',
+          value: 'Hair & Wig Design'
+        },
+        {
+          label: 'Make-up Design',
+          value: 'Make-up Design'
+        }
+      ]
+    }
+  };
+
+  const isRoleInRolesSection = (section: string[], role: string) =>
+    section.indexOf(role) > -1;
+
+  const rolesSectionChange = (
+    chkValue: any,
+    chk: string,
+    fieldName: string,
+    section: string[]
+  ) => {
+    let sectionRoles = [...section];
+
+    if (chkValue) {
+      if (sectionRoles.indexOf(chk) < 0) {
+        sectionRoles.push(chk);
+      }
+    } else {
+      sectionRoles = sectionRoles.filter(rG => rG !== chk);
+    }
+
+    const target = {
+      name: 'offstageRolesGeneral',
+      value: sectionRoles
+    };
+
+    setForm({ target });
+  };
+
   return (
     <Container>
       <Row>
         <Col lg="12">
-          <h1>So, What Do You Like Doing?</h1>
-          <h2>Tell us what positions suit you best</h2>
+          <Title>SO, WHAT DO YOU LIKE DOING?</Title>
+          <TitleSecond>Tell us what positions suit you best.</TitleSecond>
+          <TitleThird>Off-Stage Roles</TitleThird>
+          <TitleFourth>Select all applicable positions</TitleFourth>
         </Col>
       </Row>
+
+      {Object.keys(offstageRolesObj).map(objKey => {
+        const currObjKey = (offstageRolesObj as any)[objKey as any];
+        const {
+          sectionStateValue,
+          sectionStateName,
+          textHeader,
+          role,
+          checkboxes
+        } = currObjKey;
+
+        return (
+          <Row>
+            <Col lg="10">
+              <GenericAccordion
+                key={`accordion-${textHeader}`}
+                textHeader={textHeader}
+              >
+                <Row>
+                  <Col lg="7">
+                    {checkboxes.map((chk: { label: any; value: any }) => {
+                      const { label: chkLabel, value: chkValue } = chk;
+
+                      return (
+                        <Checkbox
+                          checked={isRoleInRolesSection(
+                            sectionStateValue,
+                            chk.value
+                          )}
+                          fieldType="checkbox"
+                          key={`${textHeader}-chk-${chkLabel}-${chkValue}`}
+                          label={chkLabel}
+                          onChange={(e: any) =>
+                            rolesSectionChange(
+                              e.currentTarget.checked,
+                              role,
+                              sectionStateName,
+                              sectionStateValue
+                            )
+                          }
+                          value={chkValue}
+                        />
+                      );
+                    })}
+                  </Col>
+                  <ImageCol lg="4">
+                    <Image src={yellow_blob_1} />
+                  </ImageCol>
+                </Row>
+              </GenericAccordion>
+            </Col>
+          </Row>
+        );
+      })}
     </Container>
   );
 };
+
+const Title = styled.h1`
+  font-family: ${fonts.montserrat};
+  font-size: 48px;
+`;
+
+const TitleSecond = styled.h2`
+  font-family: ${fonts.lora};
+  font-size: 28px;
+  font-weight: italic;
+`;
+
+const TitleThird = styled.h3`
+  font-family: ${fonts.mainFont};
+  font-size: 20px;
+`;
+
+const TitleFourth = styled.h4`
+  font-family: ${fonts.lora};
+  font-size: 14px;
+`;
+
+const ImageCol = styled(Col)`
+  display: flex;
+  max-height: 100%;
+  max-width: 100%;
+`;
 
 export default OffstageRoles;
---
+++ src/genericComponents/Accordion.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import Accordion from 'react-bootstrap/Accordion';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import { CaretDownFill, CaretUpFill } from 'react-bootstrap-icons';
+import styled from 'styled-components';
+
+const GenericAccordion = (props: any) => {
+  const { children, textHeader } = props;
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const arrowDirection = () => {
+    isOpen ? <CaretDownFill /> : <CaretUpFill />;
+  };
+
+  return (
+    <Accordion1 defaultActiveKey="0" onClick={() => setIsOpen(!isOpen)}>
+      <Card>
+        <CardHeader>
+          {textHeader}
+          <Accordion.Toggle as={Button} eventKey="0" variant="button">
+            {arrowDirection}
+          </Accordion.Toggle>
+        </CardHeader>
+        <Accordion.Collapse eventKey="0">
+          <Card.Body>{children}</Card.Body>
+        </Accordion.Collapse>
+      </Card>
+    </Accordion1>
+  );
+};
+
+const CardHeader = styled(Card.Header)`
+  display: flex;
+  justify-content: space-between;
+  background-color: white;
+`;
+
+const Accordion1 = styled(Accordion)`
+  box-shadow: 2px 2px 10px #00000029;
+  border-radius: 8px;
+  opacity: 1;
+`;
+
+export default GenericAccordion;
