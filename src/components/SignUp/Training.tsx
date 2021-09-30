2e05a056d8cedd7415045d297392ee02b40bd70d
jameswilson425 <jimewilson87@gmail.com>
2021-09-30 17:26:47 -0500

    training page partial changes

--- src/components/SignUp/Training.tsx
+++ src/components/SignUp/Training.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
 import { Tagline, Title } from '../layout/Titles';
+import CountrySelect from 'react-bootstrap-country-select';
+
+const [value, setValue] = useState(null);
 
 const Training: React.FC<{
   setForm: any;
@@ -14,6 +18,25 @@ const Training: React.FC<{
         <Col lg="12">
           <Title>ADD YOUR TRAINING DETAILS</Title>
           <Tagline>Tell us where you learned to do what you do.</Tagline>
+          <Form.Group controlId="formInstitution">
+            <Form.Label>Institution</Form.Label>
+            <Form.Control type="institution" />
+          </Form.Group>
+          <Tagline>Location</Tagline>
+          <CountrySelect onChange={setValue} value={value} />
+          <Form.Group controlId="formCity">
+            <Form.Label>City</Form.Label>
+            <Form.Control type="city" />
+          </Form.Group>
+          <Form.Group controlId="formDegree">
+            <Form.Label>Degree</Form.Label>
+            <Form.Control type="degree" />
+          </Form.Group>
+          <Tagline>Notes/Details</Tagline>
+          <Form.Group>
+            <Form.Label>Tell us about your training</Form.Label>
+            <Form.Control aria-label="With textarea" as="textarea" />
+          </Form.Group>
         </Col>
       </Row>
     </Container>
