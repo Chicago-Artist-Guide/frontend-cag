49aadb79ecc9d1656b1d443cba6349d1097f0b2d
Christian Walters <xian04@yahoo.com>
2021-10-20 15:53:46 -0700

    Everything in place except for blob positioning

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,9 +3,10 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import Button from 'react-bootstrap/Button';
 import { getSessionCookie } from '../../utils/session';
 import { colors } from '../../theme/styleVars';
-import { ReactComponent as Logo } from '../../images/logoPlain.svg';
+import { ReactComponent as Logo } from '../../images/cagLogo1.svg';
 
 const Header = () => {
   const [session, setSession] = useState(getSessionCookie());
@@ -26,7 +27,7 @@ const Header = () => {
   return (
     <WhiteBackNav className="container nav white-back" expand="lg" sticky="top">
       <Navbar.Brand href="/">
-        <Logo height="60px" width="70px" />
+        <Logo height="52px" width="62px" />
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
@@ -40,11 +41,18 @@ const Header = () => {
           <Nav.Link as={Link} to="/donate">
             DONATE
           </Nav.Link>
-          {showSession && (
-            <Nav.Link as={Link} to="/logout">
-              LOGOUT
-            </Nav.Link>
-          )}
+          <LogButton>
+            {!showSession && (
+              <Nav.Link as={Link} to="/login">
+                LOG IN / SIGN UP
+              </Nav.Link>
+            )}
+            {showSession && (
+              <Nav.Link as={Link} to="/logout">
+                LOGOUT
+              </Nav.Link>
+            )}
+          </LogButton>
         </Nav>
       </Navbar.Collapse>
     </WhiteBackNav>
@@ -52,7 +60,28 @@ const Header = () => {
 };
 
 const WhiteBackNav = styled(Navbar)`
-  background-color: ${colors.bodyBg};
+  background-color: ${colors.white80a};
+  max-width: 100%;
+  padding: 0.875rem 6.15vw;
+  backdrop-filter: blur(15px);
+`;
+
+const LogButton = styled(Button)`
+  height: 40px;
+  width: 183px;
+  display: flex;
+  background: ${colors.mint};
+  font: inherit;
+  font-weight: 700;
+  margin-left: 0.5rem;
+  border: 0px;
+  border-radius: 20px;
+  align-items: center;
+  justify-content: center;
+
+  .nav-link {
+    flex-shrink: 0;
+  }
 `;
 
 export default Header;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import styled from 'styled-components';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/Nav';
+import { Link } from 'react-router-dom';
+import { colors } from '../theme/styleVars';
 import PageContainer from '../components/layout/PageContainer';
 import { Tagline, Title, TitleThree } from '../components/layout/Titles';
-import WhoSquare from '../components/Home/WhoSquare';
-import SVGLayer from '../components/SVGLayer';
+// import WhoSquare from '../components/Home/WhoSquare';
+// import SVGLayer from '../components/SVGLayer';
 import yellowBlob1 from '../images/yellow_blob_1.svg';
-import homeDance from '../images/home_dance.svg';
+import yellowBlob2 from '../images/yellow_blob_2.svg';
 import greenBlob from '../images/green_blob.svg';
 import dance1 from '../images/wwww-1.svg';
 import redBlob from '../images/red_blob.svg';
@@ -16,6 +20,45 @@ import blueBlob1 from '../images/blue_blob.svg';
 import dance3 from '../images/wwww-3.svg';
 
 const Home = () => {
+  const blobs = [
+    {
+      id: 1,
+      blob: yellowBlob1,
+      zIndex: -1,
+      position: 'absolute',
+      left: -7.97,
+      top: -5.25,
+      transform: 0
+    },
+    {
+      id: 2,
+      blob: redBlob,
+      zIndex: -2,
+      position: 'absolute',
+      left: 17.55,
+      top: 6.5,
+      transform: 0
+    },
+    {
+      id: 3,
+      blob: greenBlob,
+      zIndex: -3,
+      position: 'absolute',
+      left: 63.75,
+      top: -14.3,
+      transform: 'rotate(0deg)'
+    },
+    {
+      id: 4,
+      blob: yellowBlob2,
+      zIndex: -4,
+      position: 'absolute',
+      left: 85.72,
+      top: 44.25,
+      transform: -124.79
+    }
+  ];
+
   const whoWeWorkWith = [
     {
       id: 1,
@@ -85,41 +128,94 @@ const Home = () => {
   ];
 
   return (
-    <PageContainer>
-      <Row>
-        <Col lg={8}>
-          <Title>CHICAGO ARTIST GUIDE</Title>
-          <Tagline>We make connections. You make art.</Tagline>
-          <TitleThree>WHAT WE DO</TitleThree>
-          <p className="margin-container">
-            We provide affordable and free tools to help diversify theatre in
-            Chicago.
-          </p>
-        </Col>
-        <Col lg={4}>
-          <SVGLayer blob={yellowBlob1} dancer={homeDance} />
-        </Col>
-      </Row>
-      <TitleThree className="margin-top">WHO WE WORK WITH</TitleThree>
-      <Row>
-        {whoWeWorkWith.map(who => (
-          <MarginCol key={who.id} lg={true}>
-            <WhoSquare
-              blob={who.blob}
-              dancer={who.dancer}
-              points={who.points}
-              title={who.title}
+    <>
+      <BlobRow>
+        {blobs.map(blobs => (
+          <Col key={blobs.id}>
+            <img
+              alt=""
+              //left={blobs.left}
+              //position='absolute'
+              src={blobs.blob}
+              //top={blobs.top}'rem'
+              //transform={blobs.transform}
+              z-index={blobs.zIndex}
             />
-          </MarginCol>
+          </Col>
         ))}
-      </Row>
-    </PageContainer>
+      </BlobRow>
+      <PageContainer>
+        <Row>
+          <Col lg={8}>
+            <Title>CHICAGO ARTIST GUIDE</Title>
+            <Tagline>Diversifying theater one connection at a time.</Tagline>
+            <StartButton>
+              <Nav.Link as={Link} to="/login">
+                GET STARTED
+              </Nav.Link>
+            </StartButton>
+          </Col>
+          {/* <Col lg={4}>
+					<SVGLayer blob={yellowBlob1} dancer={homeDance} />
+				</Col> */}
+        </Row>
+        <TitleThree className="margin-top">WHO WE WORK WITH</TitleThree>
+        <Row>
+          {whoWeWorkWith.map(who => (
+            <MarginCol key={who.id} lg={true}>
+              {/* <WhoSquare
+							blob={who.blob}
+							dancer={who.dancer}
+							points={who.points}
+							title={who.title}
+						/> */}
+            </MarginCol>
+          ))}
+        </Row>
+      </PageContainer>
+    </>
   );
 };
 
 const MarginCol = styled(Col)`
   margin-top: 2%;
   margin-bottom: 2%;
+`;
+
+const BlobRow = styled(Row)`
+  width: 100vw;
+  position: absolute;
+  overflow: hidden;
+  z-index: -1;
+  opacity: 0.6;
+`;
+
+// const BlobImg = styled.img`
+//   alt: "";
+//   src: ${Blob.blob};
+//   z-index: ${blobs.zIndex};
+//   position: absolute;
+//   left: ${blobs.left}rem;
+//   top: ${blobs.top}rem;
+//   transform: ${blobs.transform};
+
+// `;
+
+const StartButton = styled(Button)`
+  height: 40px;
+  width: 151px;
+  display: flex;
+  background: ${colors.slate};
+  font: inherit;
+  border: 0px;
+  border-radius: 20px;
+  align-items: center;
+  justify-content: center;
+
+  .nav-link {
+    color: #fff;
+    flex-shrink: 0;
+  }
 `;
 
 export default Home;
