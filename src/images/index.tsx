1e84c14585db9deb7741f9902b42e5f465b2e6af
Christian Walters <xian04@yahoo.com>
2021-11-03 10:59:49 -0700

    Created index files for image folders to reduce redundancy and lines used in files that reference one or more images in the folders

---
+++ src/images/index.tsx
@@ -0,0 +1,31 @@
+import blueBlob from './blue_blob.svg';
+import cagLogo from './cagLogo1.svg';
+import footerBackground from './footer-background.png';
+import greenBlob from './green_blob.svg';
+import homeDance from './home_dance.svg';
+import logoPlain from './logoPlain.svg';
+import redBlob from './red_blob.svg';
+import streamingDance from './streaming_dance.svg';
+import wawDance from './waw_dance.svg';
+import wwww1 from './wwww-1.svg';
+import wwww2 from './wwww-2.svg';
+import wwww3 from './wwww-3.svg';
+import yellowBlob1 from './yellow_blob_1.svg';
+import yellowBlob2 from './yellow_blob_2.svg';
+
+export {
+  blueBlob,
+  cagLogo,
+  footerBackground,
+  greenBlob,
+  homeDance,
+  logoPlain,
+  redBlob,
+  streamingDance,
+  wawDance,
+  wwww1,
+  wwww2,
+  wwww3,
+  yellowBlob1,
+  yellowBlob2
+};
