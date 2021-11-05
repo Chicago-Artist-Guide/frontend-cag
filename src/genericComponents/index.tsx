647a954e81f222f28f28acbcc4c30aa0fe43d804
Christian Walters <xian04@yahoo.com>
2021-11-04 20:57:47 -0700

    Created index files for more streamlined import lines

---
+++ src/genericComponents/index.tsx
@@ -0,0 +1,21 @@
+import Button from './Button';
+import Card from './Card';
+import Checkbox from './Checkbox';
+import DividerBar from './DividerBar';
+import Dropdown from './Dropdown';
+import GenericAccordion from './GenericAccordion';
+import Input from './Input';
+import PrivateLabel from './PrivateLabel';
+import RadioButton from './RadioButton';
+
+export {
+  Button,
+  Card,
+  Checkbox,
+  DividerBar,
+  Dropdown,
+  GenericAccordion,
+  Input,
+  PrivateLabel,
+  RadioButton
+};
