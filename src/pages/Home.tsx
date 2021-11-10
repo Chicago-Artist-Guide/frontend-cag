7adb1e72033eba46d0c4c382ab577446306ab06d
Alex Jewell <alex.jewell@polysign.io>
2021-11-09 19:41:13 -0600

    Add mailchimp component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,19 +2,19 @@ import React from 'react';
 import styled from 'styled-components';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Nav from 'react-bootstrap/Nav';
-import { Link } from 'react-router-dom';
-import { colors } from '../theme/styleVars';
+import PageContainer from '../components/layout/PageContainer';
+import { Tagline, Title, TitleThree } from '../components/layout/Titles';
+import WhoSquare from '../components/Home/WhoSquare';
+import SVGLayer from '../components/SVGLayer';
 import Mailchimp from '../components/Home/Mailchimp';
-import {
-  BlobBox,
-  ForArtists,
-  ForTheatres,
-  PageContainer,
-  Tagline,
-  Title
-} from '../components/layout';
-import { greenBlob, redBlob, yellowBlob1, yellowBlob2 } from '../images';
+import yellowBlob1 from '../images/yellow_blob_1.svg';
+import homeDance from '../images/home_dance.svg';
+import greenBlob from '../images/green_blob.svg';
+import dance1 from '../images/wwww-1.svg';
+import redBlob from '../images/red_blob.svg';
+import dance2 from '../images/wwww-2.svg';
+import blueBlob1 from '../images/blue_blob.svg';
+import dance3 from '../images/wwww-3.svg';
 
 const Home = () => {
   const blobs = [
@@ -49,6 +49,7 @@ const Home = () => {
   ];
 
   return (
+<<<<<<< HEAD
     <>
       <BlobBox blobs={blobs} />
       <PageContainer>
@@ -66,6 +67,42 @@ const Home = () => {
         <Mailchimp />
       </PageContainer>
     </>
+=======
+    <PageContainer>
+      <Row>
+        <Col lg={8}>
+          <Title>CHICAGO ARTIST GUIDE</Title>
+          <Tagline>We make connections. You make art.</Tagline>
+          <TitleThree>WHAT WE DO</TitleThree>
+          <p className="margin-container">
+            We provide affordable and free tools to help diversify theatre in
+            Chicago.
+          </p>
+        </Col>
+        <Col lg={4}>
+          <SVGLayer blob={yellowBlob1} dancer={homeDance} />
+        </Col>
+      </Row>
+      <TitleThree className="margin-top">WHO WE WORK WITH</TitleThree>
+      <Row>
+        {whoWeWorkWith.map(who => (
+          <MarginCol key={who.id} lg={true}>
+            <WhoSquare
+              blob={who.blob}
+              dancer={who.dancer}
+              points={who.points}
+              title={who.title}
+            />
+          </MarginCol>
+        ))}
+      </Row>
+      <Row>
+        <Col lg={12}>
+          <Mailchimp />
+        </Col>
+      </Row>
+    </PageContainer>
+>>>>>>> cf386f2 (Add mailchimp component)
   );
 };
 
