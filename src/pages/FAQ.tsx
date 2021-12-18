d70cf8fff87d385635755ecebbaf9f6c5ce6ad02
Christian <xian04@yahoo.com>
2021-12-18 13:47:07 -0800

    Added functioning hyperlinks where indicated.

--- src/pages/FAQ.tsx
+++ src/pages/FAQ.tsx
@@ -8,12 +8,17 @@ import blueBlob from '../images/blue_blob.svg';
 import streamingDance from '../images/streaming_dance.svg';
 
 const FAQ = () => {
-  const questions = [
+  const aboutQ = [
     {
       id: 1,
       question: 'Why was CAG created?',
-      answer:
-        'You can read more about our founding here. https://medium.com/chiartistguide/diversifying-and-strengthening-theater-in-chicago-2145d83806e2'
+      answer: [
+        'You can read more about our founding ',
+        <a href="https://medium.com/chiartistguide/diversifying-and-strengthening-theater-in-chicago-2145d83806e2">
+          here
+        </a>,
+        '.'
+      ]
     },
     {
       id: 2,
@@ -24,55 +29,77 @@ const FAQ = () => {
     {
       id: 3,
       question: 'Can I join?',
-      answer:
-        'We are still growing and looking for people to join our team. Email us for more information. mailto:contact@chicagoartistguide.org'
-    },
+      answer: [
+        'We are still growing and looking for people to join our team. ',
+        <a href="mailto:contact@chicagoartistguide.org">Email</a>,
+        ' us for more information.'
+      ]
+    }
+  ];
+
+  const platformQ = [
     {
-      id: 4,
+      id: 1,
       question: 'Is it free?',
       answer: 'Yes! All features are currently free on Chicago Artist Guide.'
     },
     {
-      id: 5,
+      id: 2,
       question: 'How do theater companies use our services?',
       answer:
         'Theatre companies have access to see all profiles. Not all identity information is displayed on an individual’s profile, however theatre companies can use certain filters to search for people who fit the demographic in which they are searching.'
     },
     {
-      id: 6,
+      id: 3,
       question: 'How was your website built? ',
       answer:
         'We use React for the frontend and AWS microservices for the backend.'
-    },
+    }
+  ];
+
+  const privacyQ = [
     {
-      id: 7,
+      id: 1,
       question: 'Who is able to see the profile information?',
       answer:
         'Your profile is only viewable to theater companies on the platform in relation to their production requirements. '
     },
     {
-      id: 8,
+      id: 2,
       question: 'Can users choose not to enter certain information?',
       answer:
         'Users have the option to enter information that they see fit when creating their profile. We recommend filling in as much information as possible to be considered for more roles.  You may update your information at any time. If information is missing, theater companies may contact you to provide further detail.'
     },
     {
-      id: 9,
+      id: 3,
       question: 'What if I want to know about how my data is being used?',
-      answer:
-        'Please read our Terms of Service for more details. https://www.chicagoartistguide.org/terms-of-service'
-    },
+      answer: [
+        'Please read our ',
+        <a href="https://www.chicagoartistguide.org/terms-of-service">
+          Terms of Service
+        </a>,
+        ' for more details.'
+      ]
+    }
+  ];
+
+  const identityQ = [
     {
-      id: 10,
+      id: 1,
       question: 'Will I be excluded from searches based on how I identify?',
       answer:
         'If the theatre company puts no identifiers on their posting, then everyone is eligible. There are no options to filter people by the majority identities of cis and straight. We also encourage producers not to filter roles as “white” unless it is specifically outlined by the playwright.  This means, only people in underrepresented groups will have access to roles specifically created for them.'
     },
     {
-      id: 11,
+      id: 2,
       question: 'How was the identity selection chosen?',
-      answer:
-        'Options were selected based on their relevance to casting and hiring specifically for theatre. Research, interviews, and testing were used to create the first version of our selections. Read more about our process from our Research Director here. https://medium.com/chiartistguide/our-research-method-705c7e5b7c8'
+      answer: [
+        'Options were selected based on their relevance to casting and hiring specifically for theatre. Research, interviews, and testing were used to create the first version of our selections. Read more about our process from our Research Director ',
+        <a href="https://medium.com/chiartistguide/our-research-method-705c7e5b7c8">
+          here
+        </a>,
+        '.'
+      ]
     }
   ];
 
@@ -82,7 +109,29 @@ const FAQ = () => {
         <Col lg={8}>
           <Title>FREQUENTLY ASKED QUESTIONS</Title>
           <Tagline>Find out what we're all about</Tagline>
-          {questions.map(qa => (
+          <h3>About Chicago Artist Guide</h3>
+          {aboutQ.map(qa => (
+            <div className="margin-team" key={qa.id}>
+              <TitleThree>{qa.question}</TitleThree>
+              <p>{qa.answer}</p>
+            </div>
+          ))}
+          <h3>Our Platform</h3>
+          {platformQ.map(qa => (
+            <div className="margin-team" key={qa.id}>
+              <TitleThree>{qa.question}</TitleThree>
+              <p>{qa.answer}</p>
+            </div>
+          ))}
+          <h3>Privacy</h3>
+          {privacyQ.map(qa => (
+            <div className="margin-team" key={qa.id}>
+              <TitleThree>{qa.question}</TitleThree>
+              <p>{qa.answer}</p>
+            </div>
+          ))}
+          <h3>Identity</h3>
+          {identityQ.map(qa => (
             <div className="margin-team" key={qa.id}>
               <TitleThree>{qa.question}</TitleThree>
               <p>{qa.answer}</p>
