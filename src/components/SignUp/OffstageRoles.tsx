39c72a24ddba7b007c34711bf1349eab99bfb2ef
Alex Jewell <alex.jewell@polysign.io>
2022-04-24 10:20:10 -0500

    [DEV-239] Fix Accordion (#75)

--- src/components/SignUp/OffstageRoles.tsx
+++ src/components/SignUp/OffstageRoles.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import Accordion from 'react-bootstrap/Accordion';
 import { fonts } from '../../theme/styleVars';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -211,59 +212,62 @@ const OffstageRoles: React.FC<{
           <TitleFourth>Select all applicable positions</TitleFourth>
         </Col>
       </Row>
-      {Object.keys(offstageRolesObj).map(objKey => {
-        const currObjKey = (offstageRolesObj as any)[objKey as any];
-        const {
-          sectionStateValue,
-          sectionStateName,
-          textHeader,
-          role,
-          checkboxes
-        } = currObjKey;
+      <Row>
+        <Col lg="10">
+          <StyledAccordion defaultActiveKey="0">
+            {Object.keys(offstageRolesObj).map((objKey, objI) => {
+              const currObjKey = (offstageRolesObj as any)[objKey as any];
+              const {
+                sectionStateValue,
+                sectionStateName,
+                textHeader,
+                role,
+                checkboxes
+              } = currObjKey;
 
-        return (
-          <Row key={`accordion-row-${textHeader}`}>
-            <Col lg="10">
-              <GenericAccordion
-                key={`accordion-${textHeader}`}
-                textHeader={textHeader}
-              >
-                <Row>
-                  <Col lg="7">
-                    {checkboxes.map((chk: { label: any; value: any }) => {
-                      const { label: chkLabel, value: chkValue } = chk;
+              return (
+                <GenericAccordion
+                  eventKey={objI}
+                  key={`accordion-item-${textHeader}`}
+                  textHeader={textHeader}
+                >
+                  <Row>
+                    <Col lg="7">
+                      {checkboxes.map((chk: { label: any; value: any }) => {
+                        const { label: chkLabel, value: chkValue } = chk;
 
-                      return (
-                        <Checkbox
-                          checked={isRoleInRolesSection(
-                            sectionStateValue,
-                            chk.value
-                          )}
-                          fieldType="checkbox"
-                          key={`${textHeader}-chk-${chkLabel}-${chkValue}`}
-                          label={chkLabel}
-                          onChange={(e: any) =>
-                            rolesSectionChange(
-                              e.currentTarget.checked,
-                              role,
-                              sectionStateName,
-                              sectionStateValue
-                            )
-                          }
-                          value={chkValue}
-                        />
-                      );
-                    })}
-                  </Col>
-                  <ImageCol lg="4">
-                    <Image src={yellow_blob_1} />
-                  </ImageCol>
-                </Row>
-              </GenericAccordion>
-            </Col>
-          </Row>
-        );
-      })}
+                        return (
+                          <Checkbox
+                            checked={isRoleInRolesSection(
+                              sectionStateValue,
+                              chk.value
+                            )}
+                            fieldType="checkbox"
+                            key={`${textHeader}-chk-${chkLabel}-${chkValue}`}
+                            label={chkLabel}
+                            onChange={(e: any) =>
+                              rolesSectionChange(
+                                e.currentTarget.checked,
+                                role,
+                                sectionStateName,
+                                sectionStateValue
+                              )
+                            }
+                            value={chkValue}
+                          />
+                        );
+                      })}
+                    </Col>
+                    <ImageCol lg="4">
+                      <Image src={yellow_blob_1} />
+                    </ImageCol>
+                  </Row>
+                </GenericAccordion>
+              );
+            })}
+          </StyledAccordion>
+        </Col>
+      </Row>
     </Container>
   );
 };
@@ -282,6 +286,12 @@ const ImageCol = styled(Col)`
   display: flex;
   max-height: 100%;
   max-width: 100%;
+`;
+
+const StyledAccordion = styled(Accordion)`
+  box-shadow: 2px 2px 10px #00000029;
+  border-radius: 8px;
+  opacity: 1;
 `;
 
 export default OffstageRoles;
--- src/genericComponents/GenericAccordion.tsx
+++ src/genericComponents/GenericAccordion.tsx
@@ -1,42 +1,45 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Accordion from 'react-bootstrap/Accordion';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
-import { CaretDownFill, CaretUpFill } from 'react-bootstrap-icons';
+import { CaretDownFill } from 'react-bootstrap-icons';
 import styled from 'styled-components';
 
 const GenericAccordion = (props: any) => {
-  const { children, textHeader } = props;
-  const [isOpen, setIsOpen] = useState(true);
-  const arrowDirection = isOpen ? <CaretDownFill /> : <CaretUpFill />;
+  const { children, textHeader, eventKey } = props;
 
   return (
-    <StyledAccordion defaultActiveKey="0" onClick={() => setIsOpen(!isOpen)}>
-      <Card>
-        <CardHeader>
-          {textHeader}
-          <Accordion.Toggle as={Button} eventKey="0" variant="button">
-            {arrowDirection}
-          </Accordion.Toggle>
-        </CardHeader>
-        <Accordion.Collapse eventKey="0">
-          <Card.Body>{children}</Card.Body>
-        </Accordion.Collapse>
-      </Card>
-    </StyledAccordion>
+    <CardHolder>
+      <Accordion.Collapse eventKey={eventKey || '0'}>
+        <Card.Body>{children}</Card.Body>
+      </Accordion.Collapse>
+      <CardHeader>
+        {textHeader}
+        <Accordion.Toggle
+          as={Button}
+          eventKey={eventKey || '0'}
+          variant="button"
+        >
+          <CaretDownFill />
+        </Accordion.Toggle>
+      </CardHeader>
+    </CardHolder>
   );
 };
+
+const CardHolder = styled(Card)`
+  flex-direction: column-reverse;
+  .show + .card-header {
+    button svg {
+      transform: scaleY(-1);
+    }
+  }
+`;
 
 const CardHeader = styled(Card.Header)`
   display: flex;
   justify-content: space-between;
   background-color: white;
-`;
-
-const StyledAccordion = styled(Accordion)`
-  box-shadow: 2px 2px 10px #00000029;
-  border-radius: 8px;
-  opacity: 1;
 `;
 
 export default GenericAccordion;
