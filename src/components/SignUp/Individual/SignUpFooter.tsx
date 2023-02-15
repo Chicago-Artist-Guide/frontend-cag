7f871d20727e6ea18da246841d95c157d34163e3
Alex Jewell <al3xj3w3ll@gmail.com>
2023-02-15 11:18:12 -0600

    Simplify

--- src/components/SignUp/Individual/SignUpFooter.tsx
+++ src/components/SignUp/Individual/SignUpFooter.tsx
@@ -59,11 +59,7 @@ const SignUpFooter: React.FC<{
   const prevButtonAction = (landingStep: number) => {
     goToTop();
 
-    return landingStep === 0
-      ? setLandingStep(-1)
-      : previous
-      ? previous()
-      : () => null;
+    return landingStep === 0 ? setLandingStep(-1) : previous?.();
   };
 
   const nextButtonAction = async (navNext: boolean, currStep: string) => {
