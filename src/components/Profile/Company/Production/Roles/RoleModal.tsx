bcff9513fc86f3b009d73d6659889eb9437deb4a
Alan Hoskins <alanhoskins@gmail.com>
2023-08-19 01:32:29 -0400

    add ability to delete show and role

--- src/components/Profile/Company/Production/Roles/RoleModal.tsx
+++ src/components/Profile/Company/Production/Roles/RoleModal.tsx
@@ -10,21 +10,27 @@ import { CAGLabel } from '../../../../SignUp/SignUpStyles';
 import { FormInput, FormTextArea } from '../../../Form/Inputs';
 import { StageRole } from '../../../shared/profile.types';
 import { Role } from '../../types';
-import { ageRanges, ethnicities, genders } from '../../../../../utils/lookups';
+import {
+  ageRanges,
+  ethnicities,
+  genders,
+  roleStatuses
+} from '../../../../../utils/lookups';
+import { getOptions } from '../../../../../utils/helpers';
+import ConfirmDialog from '../../../../ConfirmDIalog';
 
-const statuses = ['Open', 'Closed'].map((status) => ({
-  name: status,
-  value: status
-}));
+const statuses = getOptions(roleStatuses);
 
 const RoleModal: React.FC<{
   show: boolean;
   type: StageRole;
   role?: Role;
+  onDelete: (x: Role) => void;
   onSubmit: (x: Role) => void;
   onClose: () => void;
-}> = ({ show = false, type, role = {}, onSubmit, onClose }) => {
+}> = ({ show = false, type, role = {}, onSubmit, onClose, onDelete }) => {
   if (!show) return null;
+  const [showConfirm, setShowConfirm] = useState(false);
   const [formValues, setFormValues] = useState<Role>({});
 
   useEffect(() => {
@@ -45,7 +51,6 @@ const RoleModal: React.FC<{
       | FormTarget
   ) => {
     const eventTarget = event.target as any;
-    console.log('eventTarget', eventTarget, event);
     setFormValues((prev) => ({
       ...prev,
       [eventTarget?.name]: eventTarget?.value
@@ -61,151 +66,182 @@ const RoleModal: React.FC<{
   const isAgeRangeInAgeRanges = (ageRange: string) => {
     return formValues.age_range?.includes(ageRange);
   };
+
   const isGenderInGenders = (gender: string) => {
     return formValues.gender_identity?.includes(gender);
   };
+
   const isEthnicityInEthnicities = (ethnicity: string) => {
     return formValues.ethnicity?.includes(ethnicity);
   };
 
+  const onDeleteConfirm = () => {
+    onDelete(formValues);
+  };
+
+  const onDeleteCancel = () => {
+    setShowConfirm(false);
+  };
+
   return (
-    <Modal show={show} dialogClassName="modal-w">
-      <Modal.Body>
-        <Form className="px-5 py-4">
-          <Row>
-            <Col>
-              <Title>{title}</Title>
-            </Col>
-            <div className="d-flex flex-row flex-shrink-1 flex-row-reverse">
-              <CloseButton
-                className="d-flex align-items-center"
-                onClick={onClose}
-              >
-                <Icon icon={faClose} />
-              </CloseButton>
+    <>
+      <ConfirmDialog
+        title="Delete role"
+        content="Are you sure you wish to delete this role from the production?"
+        onCancel={onDeleteCancel}
+        onConfirm={onDeleteConfirm}
+        show={showConfirm}
+      />
+      <Modal show={!showConfirm} dialogClassName="modal-w">
+        <Modal.Body>
+          <Form className="px-5 py-4">
+            <Row>
+              <Col>
+                <Title>{title}</Title>
+              </Col>
+              <div className="d-flex flex-row flex-shrink-1 flex-row-reverse">
+                <CloseButton
+                  className="d-flex align-items-center"
+                  onClick={onClose}
+                >
+                  <Icon icon={faClose} />
+                </CloseButton>
+              </div>
+            </Row>
+            <Row className="mt-5">
+              <Col lg={7}>
+                <FormInput
+                  name="role_name"
+                  label="Role Name"
+                  onChange={setFormState}
+                  defaultValue={formValues?.role_name}
+                  style={{ marginTop: 0 }}
+                />
+                <FormTextArea
+                  name="description"
+                  label="Bio/Description"
+                  onChange={setFormState}
+                  defaultValue={formValues?.description}
+                  rows={6}
+                />
+                <Form.Group className="form-group" style={{ marginTop: 20 }}>
+                  <CAGLabel>Gender Identity</CAGLabel>
+                  {genders.map((gender) => (
+                    <Checkbox
+                      checked={isGenderInGenders(gender)}
+                      fieldType="checkbox"
+                      key={`gender_identity_${gender}`}
+                      label={gender}
+                      name={`${gender}`}
+                      onChange={(e: any) => {
+                        let genders = formValues.gender_identity || [];
+                        const selected = e.target.name;
+                        if (genders?.includes(gender) && !e.target.checked) {
+                          genders = genders?.filter(
+                            (range) => range !== gender
+                          );
+                        } else if (e.target.checked) {
+                          genders.push(selected);
+                        }
+                        setFormValues({
+                          ...formValues,
+                          gender_identity: genders
+                        });
+                      }}
+                    />
+                  ))}
+                </Form.Group>
+              </Col>
+              <Col lg={{ span: 4, offset: 1 }}>
+                <Dropdown
+                  name="role_status"
+                  label="Role Status"
+                  options={statuses}
+                  value={formValues.role_status}
+                  onChange={setFormState}
+                  style={{ marginTop: 0 }}
+                />
+                <Form.Group className="form-group" style={{ marginTop: 20 }}>
+                  <CAGLabel>Age Range</CAGLabel>
+                  {ageRanges.map((ageRange) => (
+                    <Checkbox
+                      checked={isAgeRangeInAgeRanges(ageRange)}
+                      fieldType="checkbox"
+                      key={`age_range_${ageRange}`}
+                      label={ageRange}
+                      name={`${ageRange}`}
+                      onChange={(e: any) => {
+                        let ageRanges = formValues.age_range || [];
+                        const selectedRange = e.target.name;
+                        if (
+                          ageRanges?.includes(ageRange) &&
+                          !e.target.checked
+                        ) {
+                          ageRanges = ageRanges?.filter(
+                            (range) => range !== ageRange
+                          );
+                        } else if (e.target.checked) {
+                          ageRanges.push(selectedRange);
+                        }
+                        setFormValues({
+                          ...formValues,
+                          age_range: ageRanges
+                        });
+                      }}
+                    />
+                  ))}
+                </Form.Group>
+                <Form.Group className="form-group" style={{ marginTop: 20 }}>
+                  <CAGLabel>Ethnicity</CAGLabel>
+                  {ethnicities.map((ethnicity) => (
+                    <Checkbox
+                      checked={isEthnicityInEthnicities(ethnicity)}
+                      fieldType="checkbox"
+                      key={`age_range_${ethnicity}`}
+                      label={ethnicity}
+                      name={`${ethnicity}`}
+                      onChange={(e: any) => {
+                        let ethnicities = formValues.ethnicity || [];
+                        const selectedRange = e.target.name;
+                        if (
+                          ethnicities?.includes(ethnicity) &&
+                          !e.target.checked
+                        ) {
+                          ethnicities = ethnicities?.filter(
+                            (range) => range !== ethnicity
+                          );
+                        } else if (e.target.checked) {
+                          ethnicities.push(selectedRange);
+                        }
+                        setFormValues({
+                          ...formValues,
+                          ethnicity: ethnicities
+                        });
+                      }}
+                    />
+                  ))}
+                </Form.Group>
+              </Col>
+            </Row>
+            <div className="d-flex flex-row flex-row-reverse mt-3">
+              <Button
+                onClick={() => onSave(formValues)}
+                text="Save + Close"
+                type="button"
+                variant="primary"
+              />
+              <Button
+                onClick={() => setShowConfirm(true)}
+                text="Delete"
+                type="button"
+                variant="danger"
+                className="mr-3"
+              />
             </div>
-          </Row>
-          <Row className="mt-5">
-            <Col lg={7}>
-              <FormInput
-                name="role_name"
-                label="Role Name"
-                onChange={setFormState}
-                defaultValue={formValues?.role_name}
-                style={{ marginTop: 0 }}
-              />
-              <FormTextArea
-                name="description"
-                label="Bio/Description"
-                onChange={setFormState}
-                defaultValue={formValues?.description}
-                rows={6}
-              />
-              <Form.Group className="form-group" style={{ marginTop: 20 }}>
-                <CAGLabel>Gender Identity</CAGLabel>
-                {genders.map((gender) => (
-                  <Checkbox
-                    checked={isGenderInGenders(gender)}
-                    fieldType="checkbox"
-                    key={`gender_identity_${gender}`}
-                    label={gender}
-                    name={`${gender}`}
-                    onChange={(e: any) => {
-                      let genders = formValues.gender_identity || [];
-                      const selected = e.target.name;
-                      if (genders?.includes(gender) && !e.target.checked) {
-                        genders = genders?.filter((range) => range !== gender);
-                      } else if (e.target.checked) {
-                        genders.push(selected);
-                      }
-                      setFormValues({
-                        ...formValues,
-                        gender_identity: genders
-                      });
-                    }}
-                  />
-                ))}
-              </Form.Group>
-            </Col>
-            <Col lg={{ span: 4, offset: 1 }}>
-              <Dropdown
-                name="role_status"
-                label="Role Status"
-                options={statuses}
-                value={formValues.role_status}
-                onChange={setFormState}
-                style={{ marginTop: 0 }}
-              />
-              <Form.Group className="form-group" style={{ marginTop: 20 }}>
-                <CAGLabel>Age Range</CAGLabel>
-                {ageRanges.map((ageRange) => (
-                  <Checkbox
-                    checked={isAgeRangeInAgeRanges(ageRange)}
-                    fieldType="checkbox"
-                    key={`age_range_${ageRange}`}
-                    label={ageRange}
-                    name={`${ageRange}`}
-                    onChange={(e: any) => {
-                      let ageRanges = formValues.age_range || [];
-                      const selectedRange = e.target.name;
-                      if (ageRanges?.includes(ageRange) && !e.target.checked) {
-                        ageRanges = ageRanges?.filter(
-                          (range) => range !== ageRange
-                        );
-                      } else if (e.target.checked) {
-                        ageRanges.push(selectedRange);
-                      }
-                      setFormValues({
-                        ...formValues,
-                        age_range: ageRanges
-                      });
-                    }}
-                  />
-                ))}
-              </Form.Group>
-              <Form.Group className="form-group" style={{ marginTop: 20 }}>
-                <CAGLabel>Ethnicity</CAGLabel>
-                {ethnicities.map((ethnicity) => (
-                  <Checkbox
-                    checked={isEthnicityInEthnicities(ethnicity)}
-                    fieldType="checkbox"
-                    key={`age_range_${ethnicity}`}
-                    label={ethnicity}
-                    name={`${ethnicity}`}
-                    onChange={(e: any) => {
-                      let ethnicities = formValues.ethnicity || [];
-                      const selectedRange = e.target.name;
-                      if (
-                        ethnicities?.includes(ethnicity) &&
-                        !e.target.checked
-                      ) {
-                        ethnicities = ethnicities?.filter(
-                          (range) => range !== ethnicity
-                        );
-                      } else if (e.target.checked) {
-                        ethnicities.push(selectedRange);
-                      }
-                      setFormValues({
-                        ...formValues,
-                        ethnicity: ethnicities
-                      });
-                    }}
-                  />
-                ))}
-              </Form.Group>
-            </Col>
-          </Row>
-          <div className="d-flex flex-row flex-row-reverse">
-            <Button
-              onClick={() => onSave(formValues)}
-              text="Save + Close"
-              type="button"
-              variant="primary"
-            />
-          </div>
-        </Form>
-      </Modal.Body>
-    </Modal>
+          </Form>
+        </Modal.Body>
+      </Modal>
+    </>
   );
 };
 
@@ -219,8 +255,6 @@ const Icon = styled(FontAwesomeIcon)`
   color: ${colors.lighterGrey};
 `;
 
-export default RoleModal;
-
 const Title = styled.h2`
   font-family: ${fonts.montserrat};
   font-style: normal;
@@ -230,3 +264,5 @@ const Title = styled.h2`
   letter-spacing: 0.1em;
   text-transform: uppercase;
 `;
+
+export default RoleModal;
