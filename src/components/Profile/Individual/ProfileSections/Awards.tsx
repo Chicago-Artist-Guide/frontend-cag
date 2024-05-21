13fa4b8f4652b0ceaf8ae7f3fd004b4266fa3f76
lmg <33171331+lucianamendezg@users.noreply.github.com>
2024-05-20 19:36:22 -0600

    DEV-321 Recognition won't let you add more than 3 (#166)

---
+++ src/components/Profile/Individual/ProfileSections/Awards.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { colors } from '../../../../theme/styleVars';
+import { Container } from 'styled-bootstrap-grid';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+
+const Awards: React.FC<{
+  awards: any;
+  editMode: boolean;
+}> = ({ awards, editMode }) => {
+  const awardsIsEmpty = awards.length == 0 ? true : false;
+
+  const EditVersion = () => {
+    return (
+      <div>
+        {awards.map((award: any) => (
+          <AwardContainer>
+            <Bold>{award.title}</Bold>
+            <Bold>{award.year}</Bold>
+            <FontAwesomeIcon icon={faXmark} />
+          </AwardContainer>
+        ))}
+      </div>
+    );
+  };
+
+  return (
+    <Container>
+      {editMode ? (
+        <div>
+          <EditVersion />
+        </div>
+      ) : awardsIsEmpty ? (
+        <i>Add Awards Recognition</i>
+      ) : (
+        awards.map((award: any) => (
+          <AwardContainer>
+            <Bold>{award.title}</Bold>
+            <Bold>{award.year}</Bold>
+          </AwardContainer>
+        ))
+      )}
+    </Container>
+  );
+};
+
+const AwardContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  font-weight: bold;
+`;
+
+const Bold = styled.p`
+  font-weight: bolder;
+`;
+
+export default Awards;
