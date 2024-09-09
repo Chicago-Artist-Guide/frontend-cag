fadd0e380f24864b713960a94793cd3483545b90
lmg <33171331+lucianamendezg@users.noreply.github.com>
2024-09-09 12:23:23 -0600

    FAQ Redesign (#197)
    * added ?
    
    * Changed address + edit screen
    
    * audition and off stage
    
    * changed link of button
    
    * added answers to questions
    
    * variable font

---
+++ src/components/FAQ/homeFAQ.tsx
@@ -0,0 +1,59 @@
+export const homeFAQ = {
+  about: [
+    {
+      id: 1,
+      question: '',
+      answer: [
+        "Casting and hiring haven't always celebrated the diversity of our city.",
+        <br />,
+        <br />,
+        "We're here to change that.",
+        <br />,
+        <br />,
+        "Chicago Artist Guide offers more equitable casting and hiring opportunities by removing barriers in connecting Chicago's theatre companies with local artists.",
+        <br />,
+        <br />,
+        'Chicago theaters will be able to use our platform to find artists from the communities that their productions represent and invite them directly to audition/apply, removing barriers in auditioning, casting, and hiring.'
+      ]
+    }
+  ],
+  price: [
+    {
+      id: 2,
+      question: '',
+      answer:
+        'Yes! All features are currently free on Chicago Artist Guide thanks to our generous donors and sponsors. You can support our work here.'
+    }
+  ],
+  profile: [
+    {
+      id: 3,
+      question: '',
+      answer:
+        'Any artist who is 18+ and wants to work in Chicago theatre is welcome to create a profile. We support both union and non-union workers as well as onstage and off-stage talent.'
+    }
+  ],
+  jobs: [
+    {
+      id: 4,
+      question: '',
+      answer: [
+        'We are currently invitation-only for theatre companies. Please email Executive Director, ',
+        <a href="mailto: anna@chicagoartistguide.org">Anna Schutz</a>,
+        ', if you would like to schedule a one-on-one session to learn more and walk through the setup process.'
+      ]
+    }
+  ],
+  identity: [
+    {
+      id: 5,
+      question: '',
+      answer: [
+        'If the theatre company puts no identifiers on their posting, then everyone is eligible. There are no options to filter people by the majority identities of “cis” and “straight."',
+        <br />,
+        <br />,
+        'We also encourage producers not to filter roles as “white” unless it is specifically outlined by the playwright. This means, only people in underrepresented groups will have access to roles specifically created for them.'
+      ]
+    }
+  ]
+};
