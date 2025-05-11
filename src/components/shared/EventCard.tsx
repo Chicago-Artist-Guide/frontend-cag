9f8dc096a75460ca4e12d48990ed04208d468ae1
Alex Jewell <bestfoodalex@Alexs-NewBook-Pro.local>
2025-05-10 19:12:22 -0500

    Icons TODO message

--- src/components/shared/EventCard.tsx
+++ src/components/shared/EventCard.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { colors, fonts } from '../../theme/styleVars';
 
-// SVG icons (single-color, outline)
+// TODO: Update these icons, because they're not working
 const CalendarIcon = () => (
   <svg
     width="28"
