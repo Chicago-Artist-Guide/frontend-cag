5f961d6e93e97072020026584949652870ee34e1
Alex Jewell <alex@alexjewell.com>
2026-02-02 11:32:21 -0600

    Union status fix and firestore rules

--- src/components/Matches/TalentMatchCard.tsx
+++ src/components/Matches/TalentMatchCard.tsx
@@ -245,7 +245,9 @@ export const TalentMatchCard = ({
         <div className="text-sm sm:text-base lg:grid lg:grid-cols-2 lg:gap-2">
           <div>Union(s)</div>
           <div className="font-semibold">
-            {profile.union_status?.join(', ') || 'N/A'}
+            {Array.isArray(profile.union_status)
+              ? profile.union_status.join(', ')
+              : profile.union_status || 'N/A'}
           </div>
           <div>Special Skills</div>
           <div className="font-semibold">
