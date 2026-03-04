8b15a3e54c38f639d61fa70d1a165c87845fd317
Chris Knuteson <chrisknu@users.noreply.github.com>
2026-03-03 21:58:52 -0500

    hide thing (#301)

--- src/components/Matches/MatchesPageContainer.tsx
+++ src/components/Matches/MatchesPageContainer.tsx
@@ -1,9 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useUserContext } from '../../context/UserContext';
 import { CompanyMatchList } from './CompanyMatchList';
 import { TalentMatchesFilterBar } from './TalentMatchesFilterBar';
 import { TalentMatchList } from './TalentMatchList';
-import { CompanyMatchesFilterBar } from './CompanyMatchesFilterBar';
 
 export const MatchesPageContainer = () => {
   const { account } = useUserContext();
@@ -21,13 +20,11 @@ export const MatchesPageContainer = () => {
     <div className="flex flex-col gap-6 pt-3 lg:flex-row lg:gap-12">
       {accountType !== null ? (
         <>
-          <div className="w-full flex-none lg:w-auto">
-            {accountType === 'individual' ? (
-              <CompanyMatchesFilterBar />
-            ) : (
+          {accountType === 'company' && (
+            <div className="w-full flex-none lg:w-auto">
               <TalentMatchesFilterBar />
-            )}
-          </div>
+            </div>
+          )}
           <div className="w-full flex-1">
             {accountType === 'individual' ? (
               <CompanyMatchList />
